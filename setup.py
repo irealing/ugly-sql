8db4b24fc5881dce994bce249d77103fe1555fb2
Memory_Leak <irealing@163.com>
2018-10-28 11:53:18 +0800

    feat: 新增setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+"""
+ugly-sql 数据库工具
+"""
+from setuptools import setup, find_packages
+
+__author__ = 'Memory_Leak<irealing@163.com>'
+
+setup(
+    name="ugly_sql",
+    version="0.0.1",
+    author=__author__,
+    description="ugly-sql database tool",
+    long_description=__doc__,
+    packages=find_packages(),
+)
