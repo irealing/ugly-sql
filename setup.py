d0087cb8f7dd5646a24bec56bb18bbb6c7a4241c
Memory_Leak <irealing@163.com>
2019-10-17 16:38:42 +0800

    fixed: 构建时的项目描述异常

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from setuptools import setup, find_packages
 
 __author__ = 'Memory_Leak<irealing@163.com>'
 with open('README.md', 'r') as readme:
-    doc = readme
+    doc = readme.read()
 setup(
     name="ugly_sql",
-    version="0.0.3",
+    version="0.0.4",
     author=__author__,
     description=__doc__,
     long_description=doc,
